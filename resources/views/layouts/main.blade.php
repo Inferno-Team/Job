472b6550892709d206a17c6b452a116e8aace7f2
Mohammed Alissa <infernoacm@gmail.com>
2023-08-15 22:31:50 +0300

    updates

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title></title>
-	    <script defer src="{{ asset('js/app.js') }}"  ></script>
 
-	@include('../partials.head')
+<head>
+    <title></title>
+    <script defer src="{{ asset('js/app.js') }}"></script>
+
+    @include('../partials.head')
 </head>
+
 <body>
-@include('../partials.nav')
-<br>
-@yield('content')
-<
-	@include('../partials.footer')
- 
+    @include('../partials.nav')
+    <br>
+    @yield('content')
+    @include('../partials.footer')
 </body>
-</html> 
+
+</html>
